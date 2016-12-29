881f8fb49da6b13c937d9b08e05d465bab8007ee
Eric Parsons <ericparsons@Erics-MacBook-Pro.local>
2016-12-29 16:28:27 -0500

    Passing Variable Tutorial 6 patch

--- HSDrafts_site/HSDraftsApp/views.py
+++ HSDrafts_site/HSDraftsApp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'HSDraftsApp/home.html')
+
+def contact(request):
+    return render(request, 'HSDraftsApp/basic.html', {'content':['Contact us here:', 'ecparsons42@gmail.com']})
